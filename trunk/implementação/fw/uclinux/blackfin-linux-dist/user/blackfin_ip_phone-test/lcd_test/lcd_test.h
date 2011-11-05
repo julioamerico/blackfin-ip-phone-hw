99d31d5f10c97242ee6d3f88deee431969d4bc45
romulocmendes.ufc <romulocmendes.ufc@02941b9e-1762-11df-871c-7de3c3ae1d82>
2011-11-05 19:15:48 +0000

    Inseridos no repositório as aplicações de teste do codec, expansor de IOs e LCD.

---
+++ trunk/implementação/fw/uclinux/blackfin-linux-dist/user/blackfin_ip_phone-test/lcd_test/lcd_test.h
@@ -0,0 +1,12 @@
+/*
+ * Project: BLACKFIN IP PHONE
+ * 
+ * lcd_test.h - LCD test application
+ */
+
+#ifndef BLACKFIN_IP_PHONE_LCD_TEST_H
+#define BLACKFIN_IP_PHONE_LCD_TEST_H
+
+#define BFIN_LCD_TWI_PORT	"/dev/i2c-0"
+
+#endif /* BLACKFIN_IP_PHONE_LCD_TEST_H */
