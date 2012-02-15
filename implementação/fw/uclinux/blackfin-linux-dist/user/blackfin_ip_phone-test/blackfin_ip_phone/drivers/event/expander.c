86149a70100f18e758dc3dd2c0be1dde087da387
romulocmendes.ufc <romulocmendes.ufc@02941b9e-1762-11df-871c-7de3c3ae1d82>
2012-02-15 19:00:56 +0000

    - Criados os eventos associados a todos os botões. - Para teste, foram escritas as funções fsm_st_idle e fsm_st_menu. - Escrito o código de integração entre o driver do adp5588 e a aplicação. - Escrita a função main para teste de integração entre o driver do adp5599 e a aplicação. - Testes de integração entre o driver do adp5588 e a aplicação foram realizados com sucesso!!!

---
+++ implementação/fw/uclinux/blackfin-linux-dist/user/blackfin_ip_phone-test/blackfin_ip_phone/drivers/event/expander.c
@@ -0,0 +1,123 @@
+#include <stdint.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+
+#include <linux/version.h>
+#include <linux/input.h>
+
+#include "expander.h"
+
+#define PATH_EVNT	"/dev/input/event0"
+
+#ifndef EV_SYN
+#define EV_SYN 0
+#endif
+
+static int fd;
+
+void expander_init(void)
+{
+	if ((fd = open(PATH_EVNT, O_NONBLOCK | O_RDONLY)) < 0)
+	{
+		printf("ERROR: can't find /dev/input/event0\n");
+	}
+}
+/*
+void expander_close(void)*/
+
+int expander_read(queue_t *queue)
+{
+	struct input_event ev[64];
+	int rd, index;
+	int ev_qty = 0;
+	fsm_evnt_t pressed_button;
+
+	rd = read(fd, ev, sizeof(struct input_event) * 64);
+
+	if (rd > 0)
+	{
+		for (index = 0; index < rd / sizeof(struct input_event); index++)
+		{
+			if ((ev[index].type != EV_SYN) && (ev[index].value == 1))
+			{
+				switch (ev[index].code)
+				{
+					/* Keypad Buttons */
+					case KEY_F1:
+						pressed_button = FSM_EVNT_KEYPAD_1;
+						break;
+					case KEY_F2:
+						pressed_button = FSM_EVNT_KEYPAD_2;
+            break;
+					case KEY_F3:
+            pressed_button = FSM_EVNT_KEYPAD_3;
+            break;
+					case KEY_F4:
+            pressed_button = FSM_EVNT_KEYPAD_4;
+            break;
+					case KEY_F5:
+            pressed_button = FSM_EVNT_KEYPAD_5;
+            break;
+					case KEY_F6:
+            pressed_button = FSM_EVNT_KEYPAD_6;
+            break;
+					case KEY_F7:
+            pressed_button = FSM_EVNT_KEYPAD_7;
+            break;
+					case KEY_F8:
+            pressed_button = FSM_EVNT_KEYPAD_8;
+            break;
+					case KEY_F9:
+            pressed_button = FSM_EVNT_KEYPAD_9;
+            break;
+					case KEY_KPASTERISK:
+            pressed_button = FSM_EVNT_KEYPAD_ASTERISK;
+            break;
+					case KEY_F10:
+            pressed_button = FSM_EVNT_KEYPAD_0;
+            break;
+					case KEY_DOT:
+            pressed_button = FSM_EVNT_KEYPAD_SHARP;
+            break;
+					/* Navigation Switch */
+					case KEY_1:
+            pressed_button = FSM_EVNT_NAVSWITCH_LEFT;
+						break;
+					case KEY_2:
+            pressed_button = FSM_EVNT_NAVSWITCH_RIGHT;
+						break;
+					case KEY_3:
+            pressed_button = FSM_EVNT_NAVSWITCH_DOWN;
+						break;
+					case KEY_4:
+            pressed_button = FSM_EVNT_NAVSWITCH_UP;
+						break;
+					case KEY_5:
+            pressed_button = FSM_EVNT_NAVSWITCH_SELECT;
+						break;
+					/* GP Buttons */
+					case KEY_6:
+            pressed_button = FSM_EVNT_GPBUTTON_RIGHT;
+						break;
+					case KEY_7:
+            pressed_button = FSM_EVNT_GPBUTTON_LEFT;
+						break;
+					default:
+						break;
+				}
+				queue_insert(queue, pressed_button);
+        ev_qty++;
+			}
+			
+		}
+		return ev_qty;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+
--- implementação/fw/uclinux/blackfin-linux-dist/user/blackfin_ip_phone-test/blackfin_ip_phone/main.c
+++ implementação/fw/uclinux/blackfin-linux-dist/user/blackfin_ip_phone-test/blackfin_ip_phone/main.c
@@ -6,13 +6,51 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-#include "drivers/lcd/drv_lcd.h"
-#include "drivers/lcd/lcd.h"
+#include "fsm/queue.h"
+#include "drivers/event/expander.h"
 
 int main (void)
 {
+	fsm_t fsm;
+	queue_t	event_queue;
+
+	fsm_init(&fsm);
+	queue_init(&event_queue);
+	expander_init();
+
+	while(1)
+	{
+		int ev_qty;
+		
+    fsm_evnt_t		fsm_event;
+	  fsm_state_t		fsm_state;
+/*
+		usleep(20000);
+*/
+		ev_qty = expander_read(&event_queue);
+
+		if (ev_qty)
+		{
+    	fsm_event = queue_read(&event_queue);
+    	queue_delete(&event_queue);
+
+  		fsm_state = fsm.state;
+			fsm.state = fsm.function[fsm_state](fsm_event);
+		}
+
+	}
+}
+
+
+
+
+
+
+
+/*
+	#include <string.h>	
+
 	printf("O programa está sendo executado!!\n");
 
   lcd_init();
@@ -29,4 +67,4 @@ int main (void)
   drv_lcd_close();
 
 	return 0;
-}
+}*/
