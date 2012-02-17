fd66ec32c84c44fff02d84513c79570617368a69
romulocmendes.ufc <romulocmendes.ufc@02941b9e-1762-11df-871c-7de3c3ae1d82>
2012-02-17 22:06:27 +0000

    - Inserida a função lcd_screen_idle() em fsm_st_idle(). - Criada a função fsm_error().

--- trunk/implementação/fw/uclinux/blackfin-linux-dist/user/blackfin_ip_phone-test/blackfin_ip_phone/fsm/fsm.c
+++ trunk/implementação/fw/uclinux/blackfin-linux-dist/user/blackfin_ip_phone-test/blackfin_ip_phone/fsm/fsm.c
@@ -1,4 +1,4 @@
-				/*
+/*
  * Project: BLACKFIN IP PHONE
  * 
  * fsm.c - Finite state machine.
@@ -9,6 +9,7 @@
 
 #include "fsm.h"
 #include "queue.h"
+#include "../drivers/lcd/lcd.h"
 
 void fsm_init(fsm_t *fsm)
 {
@@ -18,38 +19,46 @@ void fsm_init(fsm_t *fsm)
 	fsm->function[FSM_ST_MENU_CONTACTS]		=	fsm_st_menu_contacts;
 	fsm->function[FSM_ST_MENU_CALL_LOGS]	= fsm_st_menu_call_logs;
 	fsm->function[FSM_ST_MENU_SETTINGS]		= fsm_st_menu_settings;
+
+	fsm_st_idle(FSM_EVNT_NULL);
 }
 
 fsm_state_t fsm_st_idle(fsm_evnt_t evnt)
 {
+	char *name 			= "XUXA M.";
+	char *identity	= "1001";
+	char *time			= "17:05";
+	char *day				= "SUN, 10/05/2012";
+	char *left			= "MENU";
+	char *right			= "CONTACTS";
+
+	lcd_screen_idle(name, identity, time, day, left, right);
+
 	switch (evnt)
 	{
-		case FSM_EVNT_GPBUTTON_RIGHT:
-			return FSM_ST_IDLE;
 		case FSM_EVNT_GPBUTTON_LEFT:
-			printf("estado atual = MENU\n");
 			return FSM_ST_MENU;
+		case FSM_EVNT_GPBUTTON_RIGHT:
+			return FSM_ST_CONTACTS_LIST;
 		case FSM_EVNT_NULL:
 			return FSM_ST_IDLE;
 		default:
-			return FSM_ST_IDLE;
+			fsm_error(FSM_ST_IDLE);
 	}
 }
 
 fsm_state_t fsm_st_menu(fsm_evnt_t evnt)
-{
+{/*
   switch (evnt)
   {
-    case FSM_EVNT_GPBUTTON_RIGHT:
-			printf("estado atual = IDLE\n");
-			return FSM_ST_IDLE;
-    case FSM_EVNT_GPBUTTON_LEFT:
-			return FSM_ST_IDLE;
-		case FSM_EVNT_NULL:
-			return FSM_ST_IDLE;
+    case
+    case
+    case FSM_EVNT_NULL:
+      return FSM_ST_MENU;
     default:
-			return FSM_ST_IDLE;
+      fsm_error(FSM_ST_MENU);
   }
+*/
 }
 
 fsm_state_t fsm_st_menu_contacts(fsm_evnt_t evnt)
@@ -62,4 +71,11 @@ fsm_state_t fsm_st_menu_call_logs(fsm_evnt_t evnt)
 
 fsm_state_t fsm_st_menu_settings(fsm_evnt_t evnt)
 {
+}
+
+void fsm_error(fsm_state_t state)
+{
+	printf("ERROR!\n");
+	printf("State: %d\n", state);
+	exit(1);
 }
